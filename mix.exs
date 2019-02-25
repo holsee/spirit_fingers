cf4c6675e70d724e2284a8382ae276baf05339f1
Steven Holdsworth <sholdsworth@gmail.com>
2019-02-25 11:40:20 +0000

    Bumped to version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SpiritFingers.MixProject do
   def project do
     [
       app: :spirit_fingers,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
